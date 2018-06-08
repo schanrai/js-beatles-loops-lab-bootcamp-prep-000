7cdab0defd4ce9694c0476a6de083920add4f04b
Sushi Chanrai <sushi@pathworks.co>
2018-06-08 15:41:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function johnLennonFacts(facts){
   var newFacts = [];
   let i = 0;
   while (i<facts.length) {
-    newFacts.push(`${facts[i]}!!!`)
+    newFacts.push(`${facts[i]}!!!`);
     i++;
   }
   return newFacts;
